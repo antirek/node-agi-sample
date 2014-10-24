38523a749817270a755df82c9cd82b1854fd82c8
sergey <serge.dmitriev@gmail.com>
2014-10-24 22:13:14 +0800

    some modify

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-require('ding-dong').createServer(function(context) {
+var agiServer = require('ding-dong');
+
+
+var handler = function(context) {
   //context is a new instance of agi.Context for each new agi session
   //immedately after asterisk connects to the node process
 
   context.on('variables', function(vars) {
-    console.log('received new call from: ' + vars.agi_callerid + ' with uniqueid: ' + vars.agi_uniqueid);
+    console.log('received new call from: ' + vars.agi_callerid + 
+      ' with uniqueid: ' + vars.agi_uniqueid);
   });
 
   context.exec('ANSWER', function(err, res) {
-    console.log('call answered');
-    console.log(context);
+    console.log('call answered');    
   });
 
-  context.dial('SIP/1061', 10, function(err, res){
-    console.log('call dial');
+  var filename = '/tmp/agent-alreadyon';
+  context.streamFile(filename, function(err, res){
+    console.log(err);
+    console.log(res);
+    
   });
 
-}).listen(3007);
+  context.sayDigits('987654321', '*#', function(err, res) {
+    console.log('say number');
+    console.log(res);
+    
+  });
+  
+}
+
+agiServer.createServer(handler).listen(3007);
